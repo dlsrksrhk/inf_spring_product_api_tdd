24725d7586433d97e76ae5f7f54bc744a4cc0c49
sweet.dh <dlsrksrhk@gmail.com>
2024-11-11 17:55:47 +0900

    API 테스트로 전환

--- product-order-service/src/main/java/org/example/productorderservice/product/ProductRepository.java
+++ product-order-service/src/main/java/org/example/productorderservice/product/ProductRepository.java
@@ -1,17 +1,8 @@
 package org.example.productorderservice.product;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Repository
-class ProductRepository {
-    private Long sequnce = 0L;
-    private Map<Long, Product> persistence = new HashMap<>();
-
-    public void save(Product product) {
-        product.assisgnId(++sequnce);
-        persistence.put(product.getId(), product);
-    }
+interface ProductRepository extends JpaRepository<Product, Long> {
 }
